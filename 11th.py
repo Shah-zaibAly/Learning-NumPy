c85e6c065d92bdecc8e520e4b6998571fe982ca9
Shah-zaibAly <shahxaib269@gmail.com>
2026-07-27 18:49:07 +0500

    Shuffle and Choice - 11th

--- 11th.py
+++ 11th.py
@@ -8,3 +8,15 @@ print(rng.random(3))  # returns three random numbers between 0 and 1
 print(rng.integers(5, 101))  # 5 is inclusive and 101 is exclusive
 print(rng.integers(10, 20, size=5))  # will return 5 random numbers bw 10 and 20
 print(rng.integers(50, 90, size=(3,2)))  # will return an [3,2] array of random numbers
+
+# Shuffling 
+
+a = np.array([1,2,3,4,5])
+rng.shuffle(a)
+print(a)
+
+# Choice
+
+fruits = np.array(["Apple", "Banana", "Pineapple", "Pomegranate", "Melon", "Strawberry", "Dragon Fruit"])
+print(rng.choice(fruits))
+print(rng.choice(fruits, size=(2,2)))
