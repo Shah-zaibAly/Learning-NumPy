7af650030c3172ba160e77ec70f933b4adc93570
Shah-zaibAly <shahxaib269@gmail.com>
2026-07-27 14:26:25 +0500

    Scalar Arithmetic - 4th

---
+++ 4th.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Scalar Arithmetic
+
+array = np.array([1,2,3,4,5])
+
+print(array + 1)  # add 1 to all the numbs
+print(array - 2)  # subtract 2 from all the numbs
+print(array * 3)  # multiply by 3 to all 
+print(array / 4)  # divide all by 4
+print(array ** 5) # all raise to power 5 
