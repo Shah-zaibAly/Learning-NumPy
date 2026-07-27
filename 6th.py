4dcf512361800fe948a7e4aad8d9b209c2217f9d
Shah-zaibAly <shahxaib269@gmail.com>
2026-07-27 14:41:09 +0500

    Element-Wise Arithmetic - 6th

---
+++ 6th.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Element-Wise Arithmetic
+
+array1 = np.array([1,2,3])
+array2 = np.array([4,5,6])
+
+print(array1 + array2)
+print(array1 - array2)
+print(array1 * array2)
+print(array1 / array2)
+print(array1 ** array2)
