504ab41933543d61b6ba5f6610d241909ed7fa52
Shah-zaibAly <shahxaib269@gmail.com>
2026-07-27 13:14:59 +0500

    Learning NumPy - 1st

---
+++ 1st.py
@@ -0,0 +1,10 @@
+import numpy as np       
+     
+# Installed numpy using "pip install numpy" command
+# created an array using np.array
+
+myList = np.array([1,2,3,4])
+myList *= 2
+
+print(myList)
+print(type(myList))
